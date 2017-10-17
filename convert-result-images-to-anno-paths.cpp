ef364497052067324c3b9948c89a2ee3dcc221ba
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-10-17 19:12:19 +0300

    Add skeleton

---
+++ convert-result-images-to-anno-paths.cpp
@@ -0,0 +1,11 @@
+﻿#include <dlib/dir_nav/dir_nav_extensions.h>
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << std::endl;
+        std::cout << "> convert-result-images-to-anno-paths /path/to/result/data" << std::endl;
+        return 1;
+    }
+}
