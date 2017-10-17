1091c04261fce12763af4ea0d62c78c06fa7ec83
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-10-17 19:23:30 +0300

    Actually open the result images

--- convert-result-images-to-anno-paths.cpp
+++ convert-result-images-to-anno-paths.cpp
@@ -1,4 +1,5 @@
 ﻿#include <dlib/dir_nav/dir_nav_extensions.h>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
 
 int main(int argc, char** argv)
 {
@@ -9,8 +10,22 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    const std::vector<dlib::file> files = dlib::get_files_in_directory_tree(argv[1], dlib::match_ending("_result.png"));
+    const std::string result_image_suffix = "_result.png";
+
+    const std::vector<dlib::file> files = dlib::get_files_in_directory_tree(argv[1], dlib::match_ending(result_image_suffix));
 
     std::cout << "Searching for result images..." << std::endl;
     std::cout << "Converting " << files.size() << " result images..." << std::endl;
+
+    for (const auto& file : files) {
+        std::cout << "Converting " << file.full_name();
+
+        cv::Mat image = cv::imread(file.full_name(), cv::IMREAD_ANYDEPTH);
+
+        std::cout << " (" << image.rows << " x " << image.cols << ")";
+
+
+
+        std::cout << " - Done!" << std::endl;
+    }
 }
