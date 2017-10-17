4148d8701d082363dd4983481b4624229d34313a
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-10-17 19:17:10 +0300

    Add search for relevant result images

--- convert-result-images-to-anno-paths.cpp
+++ convert-result-images-to-anno-paths.cpp
@@ -8,4 +8,9 @@ int main(int argc, char** argv)
         std::cout << "> convert-result-images-to-anno-paths /path/to/result/data" << std::endl;
         return 1;
     }
+
+    const std::vector<dlib::file> files = dlib::get_files_in_directory_tree(argv[1], dlib::match_ending("_result.png"));
+
+    std::cout << "Searching for result images..." << std::endl;
+    std::cout << "Converting " << files.size() << " result images..." << std::endl;
 }
