aa3b758152967ead3c55b6caadc12f83e658de7d
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-10-17 20:08:57 +0300

    Find contours

--- convert-result-images-to-anno-paths.cpp
+++ convert-result-images-to-anno-paths.cpp
@@ -1,5 +1,7 @@
 ﻿#include <dlib/dir_nav/dir_nav_extensions.h>
+
 #include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 class compare_color
 {
@@ -24,6 +26,7 @@ int main(int argc, char** argv)
     }
 
     const std::string result_image_suffix = "_result.png";
+    const std::string result_path_suffix = "_result_path.json";
 
     const std::vector<dlib::file> files = dlib::get_files_in_directory_tree(argv[1], dlib::match_ending(result_image_suffix));
 
@@ -33,9 +36,12 @@ int main(int argc, char** argv)
     cv::Vec3b background_color(0, 0, 0);
 
     for (const auto& file : files) {
-        std::cout << "Converting " << file.full_name();
 
-        const cv::Mat result_image = cv::imread(file.full_name());
+        const std::string& full_name = file.full_name();
+
+        std::cout << "Converting " << full_name;
+
+        const cv::Mat result_image = cv::imread(full_name);
 
         std::cout
             << ", width = " << result_image.cols
@@ -55,9 +61,31 @@ int main(int argc, char** argv)
             }
         }
 
-        std::cout << ": " << colors.size() << " distinct classes";
+        std::cout << ", " << colors.size() << " distinct classes:" << std::endl;
 
+        const size_t base_pos = full_name.length() - result_image_suffix.length();
+        assert(base_pos < full_name.length()); // check that we didn't just wrap
+        assert(full_name.substr(base_pos) == result_image_suffix);
+        const std::string path_filename = full_name.substr(0, base_pos) + result_path_suffix;
 
-        std::cout << " - Done!" << std::endl;
+        std::ofstream out(path_filename);
+
+        cv::Mat color_result;
+        for (const cv::Vec3b& color : colors) {
+
+            std::cout << " - 0x" << std::hex 
+                << std::setw(2) << std::setfill('0') << static_cast<int>(color[2])
+                << std::setw(2) << std::setfill('0') << static_cast<int>(color[1])
+                << std::setw(2) << std::setfill('0') << static_cast<int>(color[0])
+                << ": " << std::dec;
+
+            cv::inRange(result_image, color, color, color_result);
+
+            std::vector<std::vector<cv::Point>> contours;
+            cv::findContours(color_result, contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+
+            std::cout << contours.size() << " paths" << std::endl;
+            
+        }
     }
 }
